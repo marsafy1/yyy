6765991bdb23d51adfee89d3292d8fc934e1a7c3
mokhallid80 <marsafy@marsafys-MacBook-Pro.local>
2024-01-22 14:07:27 +0300

    Getting User input

--- engine.py
+++ engine.py
@@ -15,6 +15,10 @@ ami_id = 'ami-0e35ae85b404efe33'
 instance_type = 't3.micro'
 security_group_id = 'sg-0f99030b4a237d514'
 
+# Global Variables
+current_instance = None
+running_instance_count = 0
+
 # Creating a boto3 client
 client = boto3.client('ec2',
                     region_name=region_name,
@@ -54,3 +58,47 @@ def create_new_instance():
     print(f"EC2 Instance with ID {instance_id} has been created.")
 
     return instance_id
+
+# To get ANY user input. Keep getting input till we get a valid input
+def get_user_input(input_message, validate_function):
+    user_input =  input(input_message)
+
+    try:
+        validate_function(user_input)
+    except Exception as e:
+        print(e)
+        get_user_input(input_message, validate_function)
+
+# To validate the instance choice
+def validate_instance_choice(user_choice):
+    if(str(user_choice).lower() == 'n'):
+        print('Will Create a new instance')
+    else:
+        user_choice = int(user_choice)
+        if(user_choice > running_instance_count or user_choice < 1):
+            raise Exception('Invalid number - no such instance')
+
+# To get ready and choose an instance
+def user_choose_instance():
+    print(f'You have {running_instance_count} instance(s) running')
+    user_choice = get_user_input(f'Choose one (1 - {running_instance_count}) or create a new instance(N): ', validate_instance_choice)
+
+# Main starting function
+def start():
+    global running_instance_count
+
+    running_instances_ids, not_running_instances_ids = get_existing_instances()
+    running_instance_count = len(running_instances_ids)
+
+    if(running_instance_count > 0):
+        user_choose_instance()
+    else:
+        print('You do not have any instances, will create a new one')
+
+
+
+
+
+
+if __name__ == '__main__':
+    start()
